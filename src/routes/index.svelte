18165352fe40b6a4ee06a96692e945ba84a4cf74
jakubmanczak <jakub@manczak.net>
2022-06-29 22:08:39 +0200

    DOM & styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<h1>Katakanize!</h1>
+	<p>Convert Latin script to Katakana with ease!</p>
+	<p>*Half-assedly with broken Japanese - based on phonetics.</p>
+</header>
+<main>
+	<textarea placeholder="Enter your text here..." />
+	<textarea placeholder="...And out comes katakana. ブワーラー" readonly />
+	<p class="mutedtext">
+		made by
+		<a href="https://manczak.net">jakubmanczak</a>
+	</p>
+</main>
+<!-- <section /> -->
+<!-- <footer>
+	<p>
+		made by jakubmanczak @
+		<a href="https://manczak.net">manczak.net</a>
+	</p>
+</footer> -->
