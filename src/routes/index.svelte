deb377b1789a0d8f7bf7fdbd962ac4be33070801
jakubmanczak <jakub@manczak.net>
2022-09-04 00:24:26 +0200

    add twist to alternating sides on title hover

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,20 @@
+<script lang="ts">
+	let h1: HTMLHeadingElement;
+	function swapRotDirection() {
+		h1?.classList.toggle('moused');
+	}
+</script>
+
 <header>
-	<h1>Katakanize!</h1>
+	<h1 bind:this={h1} on:mouseenter={swapRotDirection}>Katakanize!</h1>
 	<p>Convert Latin script to Katakana with ease!</p>
 	<p>*Half-assedly with broken Japanese - based on phonetics.</p>
 </header>
 <main>
 	<textarea placeholder="Enter your text here..." />
-	<textarea placeholder="...And out comes katakana. ブワーラー" readonly />
+	<textarea placeholder="...And out comes katakana. ダン" readonly />
 	<p class="mutedtext">
 		made by
 		<a href="https://manczak.net">jakubmanczak</a>
 	</p>
 </main>
-<!-- <section /> -->
-<!-- <footer>
-	<p>
-		made by jakubmanczak @
-		<a href="https://manczak.net">manczak.net</a>
-	</p>
-</footer> -->
